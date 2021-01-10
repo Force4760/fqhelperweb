39cb109aee20ef7a326a380d85e2c2dc3e89e4a0
Force4760 <gforce4760@gmail.com>
2021-01-10 14:49:56 +0000

    heroku3

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ from sidebar import sidebar
 import streamlit as st
 st.set_page_config(page_title='FQ Helper',layout = 'centered', initial_sidebar_state = 'collapsed')
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 sidebar()
